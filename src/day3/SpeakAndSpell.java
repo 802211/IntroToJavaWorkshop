0e29363f782a4f046a6a6e0b50b27f919d9b7677
william123456 <league@iMac.attlocal.net>
2015-11-04 18:28:34 -0800

    question

--- src/day3/SpeakAndSpell.java
+++ src/day3/SpeakAndSpell.java
@@ -15,16 +15,43 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
+		speak("do not spell banana, spell potato ");
 
 		// 2. Catch the user's answer in a String
+		String Answer=JOptionPane.showInputDialog("");
 
 		// 3. If the user spelled the word correctly, speak "correct"
+		if (Answer.equals("potato")){ 
+			speak("correct. I wish I could argue otherwise.");
+		}
+		else{speak("fail");
+			
+			
+		}
+		
 
 		// 4. Otherwise say "wrong"
+				speak("lima beans! Potatos! Bananas! spell lima beans");
+	
+	
+			String answer=JOptionPane.showInputDialog("");
+			
+			if(answer.equals ("lima beans")){
+				speak("someone is a banana. I don't know who! You are correct. unfortunatly");}
+					
+				else{speak("someone is a potato. not mentioning names. yay! you are incorrect!");
+				
+				
+			}
+			}
+			
+			
+		
+		
 
 		// 5. repeat the process for other words
 		
-	}
+	
 
 	static void speak(String words) {
 		try {
--- src/day3/WorldDomination.java
+++ src/day3/WorldDomination.java
@@ -5,11 +5,19 @@ import javax.swing.JOptionPane;
 public class WorldDomination {
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
+		String A=JOptionPane.showInputDialog("Do you know how to code?");
 
 		// 2. If they say "yes", tell them they will rule the world.
+		if(A.equals("Yes")){JOptionPane.showMessageDialog(null,"You will rule the world.  hopefully.");
+		
+			
+			
+		}
+		
+		
 
 		// 3. Otherwise, wish them good luck washing dishes.
-
+		if(A.equals("No")){JOptionPane.showMessageDialog(null,"Good luck washing dishes! You will not rule the world. I am certain");
 	}
-}
+}}
 
